5c5466a0f659427eefb02ef12b4e82485763de71
rdillens <remydillenseger@gmail.com>
2022-05-27 16:07:57 -0500

    restructured to use run_k function

--- k-means_cli.py
+++ k-means_cli.py
@@ -1,21 +1,15 @@
 # Python native libraries
 from statistics import mean
-# import datetime as dt
-# from path import Path
 from time import sleep
 from numpy import sort
-# import sqlalchemy
 import logging
 
 # Conda libraries
 import pandas as pd
-from sklearn.cluster import KMeans
+# from sklearn.cluster import KMeans
 # from sklearn.decomposition import PCA
-# from bokeh.models import HoverTool
 
 # External libraries
-# import mplfinance as mpf
-# import hvplot.pandas
 from fire import Fire
 import questionary
 
@@ -23,13 +17,15 @@ import questionary
 from utils.alpaca import alpaca as alp
 import utils.helpers as hf
 
-
-def run():
-    exchange = questionary.select(
-        message="Exchange",
-        choices=hf.exchange_list,
-    ).ask()
+def run(exchange=None, max_k=None):
+    # Choose Exchange if not given from command line
+    if exchange is None:
+        exchange = questionary.select(
+            message="Exchange",
+            choices=hf.exchange_list,
+        ).ask()
     logging.debug(exchange)
+    # Convert column names to strings
     table = hf.stringify_table_headers(
         pd.read_sql_table(
             table_name=exchange, 
@@ -37,44 +33,35 @@ def run():
             index_col='index',
         ).dropna()
     )
+    # Choose columns to remove from process
     cols_to_drop = questionary.checkbox(
         message="Select columns to drop",
         choices=list(table.columns),
     ).ask()
     logging.debug(f"dropping columns: {cols_to_drop}")
+    # Drop columns and scale data
     scaled_df = hf.scale_data(table.drop(columns=cols_to_drop))
     logging.debug(f"columns: {list(scaled_df.columns)}")
-    max_k = int(questionary.text(
-        message="Max K Clusters",
-        default="10",
-        validate=lambda text: text.isdigit(),
-    ).ask())
-    k_range = list(range(2,max_k))
-    inertia=[]
-    for i in k_range:
-        model = KMeans(n_clusters=i, random_state=0)
-        model.fit(scaled_df)
-        inertia.append(model.inertia_)
-    elbow_dict =         {
-            'k': k_range,
-            'inertia': inertia,
-        }
-    elbow_df = pd.DataFrame(elbow_dict)
-    # print(elbow_df)
-    # elbow_df = elbow_df.assign(choice_string=lambda df: str(df['k']) + str(df['inertia']))
-    choice_list = []
-    for i in elbow_df.index:
-        choice_string = f"k: {elbow_df.iloc[i]['k']:.0f} inertia: {elbow_df.iloc[i]['inertia']}"
-        choice_list.append(choice_string)
-    selected_k = round(elbow_df.iloc[choice_list.index(questionary.select("Choose K", choices=choice_list).ask())]['k'])
-    logging.debug(selected_k)
+    # Prompt for maximum k clusters if not given from command line
+    if max_k is None:
+        max_k = int(questionary.text(
+            message="Max K Clusters",
+            default="10",
+            validate=lambda text: text.isdigit(),
+        ).ask())
+    k_range = list(range(2,max_k+1))
+    logging.info(f"Calculate clusters from k={min(k_range)} to {max(k_range)}")
+    k_list = []
+    for i, j in enumerate(k_range):
+        k_list.append(hf.run_k(scaled_df, j))
+        print(f"k:{k_list[i]['k']} inertia:{k_list[i]['inertia']}")
+        scaled_df[k_list[i]['col_name']] = k_list[i]['clusters']
+        value_counts = scaled_df[k_list[i]['col_name']].value_counts().sort_index()
+        value_counts.index.names = ['cluster']
+        value_counts = pd.DataFrame(value_counts)
+        value_counts.columns = ['count']
 
-    model = KMeans(n_clusters=selected_k, random_state=0)
-    model.fit(scaled_df)
-    clusters = model.predict(scaled_df)
-
-    scaled_df['cluster'] = clusters
-    logging.info(scaled_df.columns.tolist())
+        print("Value Counts:\n", value_counts, value_counts.describe())
 
 
 if __name__ == "__main__":
